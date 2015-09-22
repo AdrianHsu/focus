81258185156e31fe37bde7fac77dc76a9f31a130
NTUwanderer <harveyycyang@gmail.com>
2015-09-22 23:39:10 +0800

    facebook not included

--- app/src/main/java/com/dots/focus/view/DashboardActivity.java
+++ app/src/main/java/com/dots/focus/view/DashboardActivity.java
@@ -1,12 +1,30 @@
-package com.dots.focus;
+package com.dots.focus.view;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.dots.focus.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class DashboardActivity extends ActionBarActivity {
+
+  private Dialog progressDialog;
+  static final String TAG = "DashboardActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +52,37 @@ public class DashboardActivity extends ActionBarActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  public void onLoginClick(View v) {
+    progressDialog = ProgressDialog.show(DashboardActivity.this, "", "Logging in...", true);
+
+    // "public_profile, email, user_status, user_friends, user_about_me, user_location"
+    List<String> permissions = Arrays.asList("public_profile, user_friends");
+    Log.d(TAG, Arrays.toString(permissions.toArray()));
+
+    // for extended permissions, like "user_about_me", your app must be reviewed by Facebook team
+    // (https://developers.facebook.com/docs/facebook-login/permissions/)
+    ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
+      @Override
+      public void done(ParseUser user, ParseException err) {
+        progressDialog.dismiss();
+        if (user == null) {
+          Log.d(TAG, "Uh oh. The user cancelled the Facebook login.");
+        } else if (user.isNew()) {
+          Log.d(TAG, "User signed up and logged in through Facebook!");
+          showUserDetailsActivity();
+        } else {
+          Log.d(TAG, "User logged in through Facebook!");
+          Log.d(TAG, AccessToken.getCurrentAccessToken().getPermissions().toString());
+          showUserDetailsActivity();
+        }
+      }
+    });
+  }
+
+  private void showUserDetailsActivity() {
+    Intent intent = new Intent(this, UserDetailsActivity.class);
+    startActivity(intent);
   }
 }
