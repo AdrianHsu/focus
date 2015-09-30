a097cd47c15d4214f2ec2165ed50885754b4b816
NTUwanderer <harveyycyang@gmail.com>
2015-09-30 20:54:39 +0800

    category done

--- app/src/main/java/com/dots/focus/ui/LoginActivity.java
+++ app/src/main/java/com/dots/focus/ui/LoginActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import com.dots.focus.R;
 import com.dots.focus.controller.LoginController;
 
+import com.dots.focus.service.GetAppsService;
 import com.parse.LogInCallback;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseException;
@@ -92,104 +93,9 @@ public class LoginActivity extends BaseActivity {
                 }
             }
         });
-        Log.d(TAG, "Service starts.");
-        JSONObject obj = new JSONObject(), apps = new JSONObject();
-
-        JSONArray list = new JSONArray();
-
-        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> ril = getPackageManager().queryIntentActivities(mainIntent, 0);
-        for (ResolveInfo ri : ril) {
-            //applicationList.add(new AppInfo(LoginActivity.this, ri));
-            String name = ri.resolvePackageName;
-            try{
-                if (ri.activityInfo != null) {
-                    Resources res = getPackageManager().getResourcesForApplication(ri.activityInfo.applicationInfo);
-
-                    AssetManager assets = res.getAssets();
-                    DisplayMetrics metrics = res.getDisplayMetrics();
-                    Configuration config = new Configuration(res.getConfiguration());
-                    config.locale = Locale.US;
-
-                    Resources engRes = new Resources(assets, metrics, config);
-
-                    if (ri.activityInfo.labelRes != 0) {
-                        name = engRes.getString(ri.activityInfo.labelRes);
-
-                        if (name == null || name.equals("")) {
-                            name = res.getString(ri.activityInfo.labelRes);
-                        }
-
-                    } else {
-                        name = ri.activityInfo.applicationInfo.loadLabel(getPackageManager()).toString();
-                    }
-                }
-            } catch(Exception e){
-                e.printStackTrace();
-            }
-            list.put(name);
-            Log.d(TAG, name);
-        }
-
-
-        try {
-            for (int i = 0; i < list.length(); i++) {
-                Log.d(TAG, "The " + i + "st: " + list.getString(i));
-            }
-            obj.put("packages", list);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Log.d(TAG, "Stage 1.");
-        HttpClient httpClient = new DefaultHttpClient(); //Deprecated
-        try {
-
-            Log.d(TAG, "Stage 1-1.");
-            HttpPost httpPost = new HttpPost("http://getdatafor.appspot.com/data");
-            httpPost.setHeader("Content-type", "application/json");
-            StringEntity params = new StringEntity(obj.toString());
-            httpPost.setEntity(params);
-            Log.d(TAG, "Stage 1-2.");
-            HttpResponse lResp = httpClient.execute(httpPost);
-
-            ByteArrayOutputStream lBOS = new ByteArrayOutputStream();
-            String lInfoStr = null;
-            JSONObject categoryResponse = null;
-            Log.d(TAG, "Stage 1-3.");
-            lResp.getEntity().writeTo(lBOS);
-            lInfoStr = lBOS.toString("UTF-8");
-            categoryResponse = new JSONObject(lInfoStr);
-            JSONArray appArr = categoryResponse.getJSONArray("apps");
-
-            Log.d(TAG, "Stage 2.");
-            for (int i = 0; i < appArr.length(); i++) {
-                JSONObject appObj = appArr.getJSONObject(i);
-                String packageVal = appObj.optString("package", null);
-                String categoryVal = appObj.optString("category", null);
-
-                if (packageVal == null || categoryVal == null)
-                    continue;
-                apps.put(packageVal, categoryVal);
-                /*
-                for(int n = 0; n < appNames.size(); n++) {
-                    if (packageVal.equalsIgnoreCase(appNames.get(i))) {
-
-                    }
-                }
-                */
-            }
-            Log.d(TAG, "Stage 3.");
-            JSONArray all = apps.names();
-            for (int i = 0; i < all.length(); i++) {
-                Log.d("GetAppsService", all.getString(i) + ": " + apps.getString(all.getString(i)));
-            }
-            Log.d(TAG, "Stage 4.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            httpClient.getConnectionManager().shutdown();
-        }
+        Log.d(TAG, "Service starts...");
+        Intent intent = new Intent(this, GetAppsService.class);
+        startService(intent);
     }
 
     private void showIntroActivity() {
@@ -203,8 +109,6 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void showDashboardActivity() {
-
-
         Intent intent = new Intent(this, DashboardActivity.class);
         startActivity(intent);
     }
