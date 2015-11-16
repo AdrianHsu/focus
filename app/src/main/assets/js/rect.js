ed430f193b9db476c33cb24eec593994889f9696
AdrianHsu <adrianhsu1995@gmail.com>
2015-11-16 13:38:04 +0800

    rect fragment

--- app/src/main/assets/js/rect.js
+++ app/src/main/assets/js/rect.js
@@ -1,8 +1,9 @@
-var svg_height = 400;
-var svg_width = 1000;
-var bar_padding = 5;
+var svg_height = screen.height * 0.75;
+var svg_width = screen.width * 0.95;
+var a = svg_height / 60;
+var bar_padding = 1;
 
-var dataset = [30, 20, 10, 40, 45, 25, 15, 35, 18];
+var dataset = [30, 35, 25, 10];
 
 var svg = d3.select("body")
     .append("svg")
@@ -17,14 +18,14 @@ svg.selectAll("rect")
         return i * (svg_width / dataset.length);
     })
     .attr("y", function(d) {
-        return svg_height - (d * 4);
+        return svg_height - (d * a);
     })
     .attr("width", svg_width / dataset.length - bar_padding)
     .attr("height", function(d) {
-        return d * 4;
+        return d * a;
     })
     .attr("fill", function(d) {
-        return "rgba(0, 100, " + d * 5 + ", 0.95)";
+        return "rgba(0, 100, " + d * 5 + ", 0.75)";
     });
  //    .on("click", function(d) {
  //          rectClick(this, d);
@@ -60,7 +61,7 @@ svg.selectAll("rect")
 // 	.enter()
 // 	.append("text")
 // 	.text(function(d) {
-// 		return d;
+// 		return d + "%";
 // 	})
 // 	.attr("text-anchor", "right")
 // 	.attr("x", function(d, i) {
@@ -71,6 +72,5 @@ svg.selectAll("rect")
 // 		return svg_height - (d * 4) + 20;
 // 	})
 // 	.attr("fill", "#FFFFFF")
-// 	.attr("font-family", "sans-serif")
-// 	.attr("font-size", "12px");
-	// .attr("fill", "#FFFFFF");
+// 	.attr("font-size", "14px");
+// 	.attr("fill", "#FFFFFF");
