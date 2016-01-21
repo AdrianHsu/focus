a7d5452c33ebbfa50478506761633b16a4926551
AdrianHsu <adrianhsu1995@gmail.com>
2016-01-21 23:28:57 +0800

    discuss almost done, with window move to top while edittext clicked not yet implemented

--- app/src/main/java/com/dots/focus/ui/KickMessagesActivity.java
+++ app/src/main/java/com/dots/focus/ui/KickMessagesActivity.java
@@ -4,80 +4,92 @@ package com.dots.focus.ui;
  * Created by AdrianHsu on 2016/1/21.
  */
 
-import java.util.Random;
+import java.util.ArrayList;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
 
 import com.dots.focus.R;
-import com.dots.focus.adapter.DiscussArrayAdapter;
-import com.dots.focus.model.OneComment;
+import com.dots.focus.adapter.DiscussRecyclerViewAdapter;
+import com.marshalchen.ultimaterecyclerview.UltimateRecyclerView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class KickMessagesActivity extends BaseActivity {
-  private DiscussArrayAdapter adapter;
-  private ListView lv;
+
   private EditText editText1;
-  private static Random random;
+
+  private UltimateRecyclerView mRecyclerView;
+  private DiscussRecyclerViewAdapter discussRecyclerViewAdapter = null;
+  private LinearLayoutManager linearLayoutManager;
+  final ArrayList<JSONObject> messages = new ArrayList<>();
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_discuss);
 
-
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
     toolbar.bringToFront();
+    mRecyclerView = (UltimateRecyclerView) findViewById(R.id.discuss_recycler_view);
+    for(int i = 0; i < 100; i++) {
+      JSONObject message = new JSONObject();
+      try {
+        message.put("text", "這是測試文字：" + String.valueOf(i));
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
 
-    random = new Random();
+      messages.add(message);
+    }
 
-    lv = (ListView) findViewById(R.id.listView1);
+    discussRecyclerViewAdapter = new DiscussRecyclerViewAdapter( messages, this);
+    linearLayoutManager = new LinearLayoutManager(this);
 
-    adapter = new DiscussArrayAdapter(getApplicationContext(), R.layout.listitem_discuss);
-
-    lv.setAdapter(adapter);
+    mRecyclerView.setLayoutManager(linearLayoutManager);
+    mRecyclerView.setAdapter(discussRecyclerViewAdapter);
+    mRecyclerView.scrollVerticallyToPosition(messages.size() - 1);
 
     editText1 = (EditText) findViewById(R.id.editText1);
+    editText1.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mRecyclerView.scrollVerticallyToPosition(messages.size() - 1);
+      }
+    });
     editText1.setOnKeyListener(new OnKeyListener() {
       public boolean onKey(View v, int keyCode, KeyEvent event) {
         // If the event is a key-down event on the "enter" button
+
         if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
           // Perform action on key press
-          adapter.add(new OneComment(false, editText1.getText().toString()));
+          JSONObject tmp = new JSONObject();
+          try {
+            tmp.put("text", editText1.getText().toString());
+          } catch( JSONException e ) {
+            e.printStackTrace();
+          }
+          messages.add(tmp);
+          mRecyclerView.getAdapter().notifyItemInserted(messages.size() - 1);
+          mRecyclerView.scrollVerticallyToPosition(messages.size() - 1);
+
           editText1.setText("");
+
           return true;
         }
         return false;
       }
     });
 
-    addItems();
   }
-
-  private void addItems() {
-    adapter.add(new OneComment(true, "這是測試文字"));
-
-    for (int i = 0; i < 4; i++) {
-      boolean left = getRandomInteger(0, 1) == 0 ? true : false;
-      String words = "愛上一匹野馬、可我的家裡沒有草原：" + String.valueOf(i);
-
-      adapter.add(new OneComment(left, words));
-    }
-  }
-
-  private static int getRandomInteger(int aStart, int aEnd) {
-    if (aStart > aEnd) {
-      throw new IllegalArgumentException("Start cannot exceed End.");
-    }
-    long range = (long) aEnd - (long) aStart + 1;
-    long fraction = (long) (range * random.nextDouble());
-    int randomNumber = (int) (fraction + aStart);
-    return randomNumber;
-  }
-
 }
