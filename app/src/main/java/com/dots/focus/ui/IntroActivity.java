9be1fab824f2c9a640116d8043b998cc08554072
AdrianHsu <adrianhsu1995@gmail.com>
2016-02-12 16:12:32 +0800

    done intro text

--- app/src/main/java/com/dots/focus/ui/IntroActivity.java
+++ app/src/main/java/com/dots/focus/ui/IntroActivity.java
@@ -16,30 +16,37 @@ public class IntroActivity extends AppIntro2 {
   @Override
   public void init(Bundle savedInstanceState) {
 
-    int image = R.drawable.ic_slide1;
+    int image0 = R.drawable.intro_0;
+    int image1 = R.drawable.intro_1;
+    int image2 = R.drawable.intro_2;
+    int image3 = R.drawable.intro_3;
+
 
     // Instead of fragments, you can also use our default slide
     // Just set a title, description, background and image. AppIntro will do the rest
 //    addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
-    addSlide(AppIntroFragment.newInstance("First", "Welcome to first page Introduction",
-      image, ContextCompat.getColor(this, R.color.blue)));
-    addSlide(AppIntroFragment.newInstance("Second", "Welcome to second page Introduction",
-      image, ContextCompat.getColor(this, R.color.green)));
-    addSlide(AppIntroFragment.newInstance("Third", "Welcome to third page Introduction",
-      image, ContextCompat.getColor(this, R.color.yellow)));
-    addSlide(AppIntroFragment.newInstance("Fourth", "Welcome to fourth page Introduction",
-      image, ContextCompat.getColor(this, R.color.red)));
+    addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_0),getResources
+                                                    ().getString(R.string.intro_0_des),
+      image0, ContextCompat.getColor(this, R.color.blue)));
+    addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_1),getResources
+                                                    ().getString(R.string.intro_1_des),
+      image1, ContextCompat.getColor(this, R.color.green)));
+    addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_2),getResources
+                                                    ().getString(R.string.intro_2_des),
+      image2, ContextCompat.getColor(this, R.color.yellow)));
+    addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.intro_3),getResources
+                                                    ().getString(R.string.intro_3_des),
+      image3, ContextCompat.getColor(this, R.color.red)));
 
 
     // Hide Skip/Done button
 //    showSkipButton(true);
     showDoneButton(true);
     setDepthAnimation();
-
     // Turn vibration on and set intensity
     // NOTE: you will probably need to ask VIBRATE permission in Manifest
     setVibrate(true);
-    setVibrateIntensity(50);
+    setVibrateIntensity(100);
   }
 //  @Override
 //  public void onSkipPressed() {
