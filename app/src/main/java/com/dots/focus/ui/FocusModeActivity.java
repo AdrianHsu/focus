8cd8767bd11e897394e64213bdd6916675537c9d
unknown <>
2016-02-18 13:09:49 +0800

    123

--- app/src/main/java/com/dots/focus/ui/FocusModeActivity.java
+++ app/src/main/java/com/dots/focus/ui/FocusModeActivity.java
@@ -1,8 +1,12 @@
 package com.dots.focus.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -17,6 +21,18 @@ import com.dots.focus.service.FocusModeService;
  */
 public class FocusModeActivity extends BaseActivity {
   private BaseActivity nowActivity;
+  private static String TAG = "FocusModeActivity";
+
+  private final BroadcastReceiver receiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      if (action.equals("resume lock mode")) {
+        Log.d(TAG, "resume lock mode...");
+        restartActivity();
+      }
+    }
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +53,41 @@ public class FocusModeActivity extends BaseActivity {
         Intent i = new Intent(nowActivity, FocusModeService.class);
         i.setAction(FocusModeService.LOCK_ACTION);
         startService(i);
-				Toast.makeText(nowActivity, "開始專注模式！", Toast.LENGTH_SHORT).show();
+        Toast.makeText(nowActivity, "開始專注模式！", Toast.LENGTH_SHORT).show();
         finish();
       }
     });
+
+    IntentFilter filter = new IntentFilter();
+    filter.addAction("resume lock mode");
+
+    registerReceiver(receiver, filter);
   }
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     onBackPressed();
     return true;
+  }
+
+  private void restartActivity() {
+    onResume();
+  }
+
+  @Override
+  protected void onResume() {
+    Log.d(TAG, "onResume called");
+    super.onResume();
+  }
+
+  @Override
+  protected void onStop() {
+    Log.d(TAG, "onStop called");
+    super.onStop();
+  }
+
+  @Override
+  protected void onDestroy() {
+    Log.d(TAG, "onDestroy called");
+    super.onDestroy();
   }
 }
