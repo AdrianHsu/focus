0f93ae065a1b8b5c3084150300e0504986fa11b3
unknown <>
2016-03-02 17:33:02 +0800

    tracker

--- app/src/main/java/com/dots/focus/application/MainApplication.java
+++ app/src/main/java/com/dots/focus/application/MainApplication.java
@@ -2,6 +2,7 @@ package com.dots.focus.application;
 
 import android.app.Application;
 
+import com.dots.focus.R;
 import com.dots.focus.config.Config;
 import com.dots.focus.model.DayBlock;
 import com.dots.focus.model.HourBlock;
@@ -10,6 +11,8 @@ import com.dots.focus.util.SettingsUtil;
 import com.dots.focus.util.TimePoliceUtil;
 import com.facebook.FacebookSdk;
 import com.flurry.android.FlurryAgent;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseObject;
@@ -18,6 +21,7 @@ import com.parse.ParseObject;
 public class MainApplication extends Application {
 
   static final String TAG = "MainApplication";
+  private Tracker tracker;
 
   @Override
   public void onCreate() {
@@ -28,19 +32,34 @@ public class MainApplication extends Application {
     ParseObject.registerSubclass(DayBlock.class);
     ParseObject.registerSubclass(HourBlock.class);
     Parse.initialize(this,
-      Config.FOCUS_APPLICATION_ID,
-      Config.FOCUS_CLIENT_ID
+            Config.FOCUS_APPLICATION_ID,
+            Config.FOCUS_CLIENT_ID
     );
 
     FlurryAgent.init(this, Config.FLURRY_KEY);
 
     ParseFacebookUtils.initialize(this);
     initialize();
+
+    getDefaultTracker();
   }
   private void initialize() {
     SettingsUtil.searchSettings();
     FetchAppUtil.loadParseApps();
 
+  }
+
+
+  synchronized public Tracker getDefaultTracker() {
+    if (tracker == null) {
+      GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+      // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+      tracker = analytics.newTracker(R.xml.tracker_config);
+      tracker.enableExceptionReporting(true);
+      tracker.enableAdvertisingIdCollection(true); // ??
+      tracker.enableAutoActivityTracking(true); // ??
+    }
+    return tracker;
   }
 }
 
